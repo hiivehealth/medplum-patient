da00622da4392969ebb131f7fd6ea390682af012
Paul Winterling <pwinterling@hiivehealth.com>
2026-05-26 14:39:35 -0400

    Sync local updates after remote rebase

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright Orangebot, Inc. and Medplum contributors
 // SPDX-License-Identifier: Apache-2.0
 import type { JSX } from 'react';
-import HiiveHealthLogo from '../img/hiive-health-logo.svg';
+import HiiveHealthLogo from '../img/hiivehealth.svg';
 
 export interface LogoProps {
   readonly width: number;
